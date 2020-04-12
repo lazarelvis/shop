a64941a596180fbf7a07e96390cdf13fb3d60432
PreotulKazoo <60981909+PreotulKazoo@users.noreply.github.com>
2020-04-12 22:53:15 +0300

    Buton Login

---
+++ js files/login.js
@@ -0,0 +1,13 @@
+$('.toggle-login').click(function(){
+    $('.login').toggle();
+  });
+  
+  $(document).mouseup(function (e) {
+      var container = $(".login");
+      if (!container.is(e.target)
+          && container.has(e.target).length == 0) 
+      {
+          container.fadeOut();
+      }
+  });
+  
--- js files/wrap.js
+++ js files/wrap.js
@@ -1,3 +1,4 @@
+
 let mainNav = document.getElementById('js-menu');
 let navBarToggle = document.getElementById('js-navbar-toggle');
 
