3716f3035b7b2391a590f80edb623b8ead4d32b8
creatina98 <34490820+creatina98@users.noreply.github.com>
2020-05-16 21:31:44 +0300

    cel mai tare search

--- navbar.php
+++ navbar.php
@@ -22,7 +22,14 @@
               <li id="oferte" class="da"><a href="oferte.php" class="nav-links"><?php echo $_SESSION['idUsername']?></a></li>
             <?php } else{ ?>
             <?php } ?>
-            <li id="meniu" class="right nu"><input type="text" placeholder="     Cauta in magazin..."></li>
+            <li id="meniu" class="right nu"><input type="text" placeholder="     Cauta in magazin...">
+                <button type="submit" name="header_search_submit" class="button-reset color-inherit db o-60 absolute center-v right-1 hover-primary6" style="border: none; background-color: white; font-size: 18px; position:absolute; right: 200px; top:55px; color: #8E78FF ">
+                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-fw">
+                        <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class="">
+                        </path>
+                    </svg>
+                </button>
+            </li>
             <li class="nav-links"><a href="#" ><i class="fas fa-user button toggle-login"></i></a></li>
             <div class="login">
                 <div class="triangle"></div><!-- .triangle -->
@@ -48,23 +55,28 @@
                     <?php } ?>
                 </form>
             </div>
-
-            <li  class="nav-links"><div id="circleCos" class="circlePosition"  >
+            <li  class="nav-links">
+                <?php
+                if (isset($_SESSION['idUtilizator'])) {
+                ?>
+                <div id="circleCos" class="circlePosition"  >
                     <h6 style=" position: absolute; color: white; left: 5px; font-weight: normal;">
                         <?php
-                        $sql1 = "SELECT COUNT(id_produse) as produse FROM cos;";
-                        $result = mysqli_query($conn,$sql1);
-                        $resultCheck = mysqli_num_rows($result);
-                        while ($row = mysqli_fetch_assoc($result)){
-                            if ($row['produse'] != 0) {
-                                echo $row['produse'];
-                            }elseif ($row['produse']==0){
-                                echo "<style> .circlePosition{display: none; }</style>";
-                            }
+                            $sql1 = "SELECT COUNT(id_produse) as produse FROM cos where id_utilizator=".$_SESSION['idUtilizator'];
+                            $result = mysqli_query($conn, $sql1);
+                            $resultCheck = mysqli_num_rows($result);
+                            while ($row = mysqli_fetch_assoc($result)) {
+                                if ($row['produse'] != 0) {
+                                    echo $row['produse'];
+                                } elseif ($row['produse'] == 0) {
+                                    echo "<style> .circlePosition{display: none; }</style>";
+                                }
                         }
                         ?>
                     </h6>
-                </div><a href="cosCumparaturi.php"><i class="fas fa-shopping-basket"></i></a></li>
+                </div>
+                <?php } ?>
+                <a href="cosCumparaturi.php"><i class="fas fa-shopping-basket"></i></a></li>
         </ul>
     </div>
 </div>
